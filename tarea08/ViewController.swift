269b1c4867c680b4d334c25ac650c3c3480af1f2
Mireya ESthefany Apaza Cuicapuza <mireya.apaza@tecsup.edu.pe>
2024-06-06 11:33:14 -0500

    Initial Commit

---
+++ tarea08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tarea08
+//
+//  Created by Mireya Esthefany Apaza Cuicapuza  on 6/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
